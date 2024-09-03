2858265b30dace319243e909818b6c10958bc7db
mohon sarker <mohonsarker40@gmail.com>
2024-09-03 11:43:25 +0600

    size added

--- main.js
+++ main.js
@@ -3,9 +3,19 @@ const app = Vue.createApp({
         return {
             product: 'Sucks',
             image: './assets/images/socks_blue.jpg',
-            url: 'https://olympics.com/en/paris-2024/paralympic-games/sports/wheelchair-tennis',
-            inStock: true,
-            outofStock: true
+            inventory: 11,
+            onSale: true,
+            details: ['50% cotton, 30% wool, 20% polyester'],
+            variants: [
+                {id: 442, color: 'Blue'},
+                {id: 443, color: 'Greeen'}
+
+            ],
+            sizes: [
+                {id: 222, fit: 'sm'},
+                {id: 333, fit: 'xl'},
+                {id: 333, fit: 'xxl'},
+            ],
         }
     }
 })
